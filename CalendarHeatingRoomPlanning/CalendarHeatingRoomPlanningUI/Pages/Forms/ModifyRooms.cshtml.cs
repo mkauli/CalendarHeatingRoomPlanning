1f6f55e2e81437cb07695466f273b7a2526910cb
Martin Kaul <martin@familie-kaul.de>
2021-05-09 22:06:01 +0200

    Insert room management

---
+++ CalendarHeatingRoomPlanning/CalendarHeatingRoomPlanningUI/Pages/Forms/ModifyRooms.cshtml.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CalendarHeatingRoomPlanningUI.Pages.Forms
+{
+    /// <summary>
+    /// This page can be used by user to modify/change the specified rooms.
+    /// </summary>
+    public class ModifyRoomsModel : PageModel
+    {
+        /// <summary>
+        /// Rooms that are already specified.
+        /// </summary>
+        [BindProperty]
+        public CalendarHeatingRoomPlanning.RoomModel Rooms
+        {
+            get { return RoomManager.Instance.Rooms;  }
+            set { }
+        }
+ 
+        public IActionResult OnGet()
+        {
+            if (!Authentification.Instance.IsAuthentificated(HttpContext))
+            {
+                return new RedirectToPageResult("/Index");
+            }
+
+            return Page();
+        }
+    }
+}
